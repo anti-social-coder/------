0ecbec13a118a9a6e6b335b782a6ebc05af69223
anti-social-coder <nathanielmcqueen2021@gmail.com>
2025-07-10 15:15:19 -0400

    Commit

--- opening.js
+++ opening.js
@@ -1,8 +1,17 @@
+//this is an alert
+ 
 window.alert("----WARNING----");
 window.alert("Your accessing a highly Classified site");
 window.alert("Proceed with caution");
 window.alert("You will be logged");
 
-//this is an alert
+let firstname = "John";
+let lastname = "Doe";
+let fullname = firstname + " " + lastname;
+console.log ('your name is ${fullname}');
+console.log(fullname);
 
+let onlinefirst = true
+
+ 
 
