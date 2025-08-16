fa0f12af724ef10ccfedc61a3733143315b1f4cd
anti-social-coder <nathanielmcqueen2021@gmail.com>
2025-08-16 15:47:12 -0400

    Commit

---
+++ clock.js
@@ -0,0 +1,25 @@
+const WEEK = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+function updateTime() {
+  const now = new Date();
+
+  document.getElementById("time").innerText =
+    zeroPadding(now.getHours(), 2) + ":" +
+    zeroPadding(now.getMinutes(), 2) + ":" +
+    zeroPadding(now.getSeconds(), 2);
+
+  document.getElementById("date").innerText =
+    now.getFullYear() + "-" +
+    zeroPadding(now.getMonth() + 1, 2) + "-" +
+    zeroPadding(now.getDate(), 2) + " " +
+    WEEK[now.getDay()];
+}
+
+function zeroPadding(num, digit) {
+  return String(num).padStart(digit, '0');
+}
+
+// Run once at start
+updateTime();
+// Update every second
+setInterval(updateTime, 1000);
