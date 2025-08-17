1c3dabe20e1e801840c07fad74c90145d1207b52
anti-social-coder <nathanielmcqueen2021@gmail.com>
2025-08-17 19:23:41 -0400

    Commit

--- opening.js
+++ opening.js
@@ -1,10 +1,10 @@
 //this is an alert
 window.alert("----WARNING----\nYour accessing a highly Classified site\nProceed with caution\nYou will be logged");
 
-let fullname = window.prompt("Please enter your first name and last name");
-console.log(fullname)
-window.alert("Welcome " + fullname)
-console.log(fullname);
+let username = window.prompt("Please enter your username");
+console.log(username)
+window.alert("Welcome " + username)
+console.log(username);
 
   
 
